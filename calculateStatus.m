b29508e2d61b5cab6a3c5b03616dcb0611d7c055
Alkaidwood <zhanghaopeng@mail.nwpu.edu.cn>
2025-09-03 16:48:03 +0200

    Add calculateStatus.m

---
+++ calculateStatus.m
@@ -0,0 +1,29 @@
+%% calculateStatus
+% This is a customized function to calculate the acceleration, speed,
+% angular at time point "tn"
+%% Syntax
+% [ddomega, domega, omega] = calculateStatus(tn)
+%% Description
+% tn: is the n-th time (s)
+% 
+% ddomega: 1*n, is the acceleration at the n-th time
+% 
+% domega: 1*n, is the speed at the n-th time
+% 
+% omega: 1*n, is the angular at the n-th time
+
+
+function [ddomega, domega, omega] = calculateStatus(tn)
+
+if tn <= 10
+   ddomega = [21         25.2];
+   domega  = ddomega .* tn ;
+   omega   = 0.5 * ddomega * tn^2;
+else
+   ddomega = zeros(1,2);
+   domega  = [210  252];
+   omega   = [1050  1260] + domega .* (tn-10);
+end
+
+
+end
